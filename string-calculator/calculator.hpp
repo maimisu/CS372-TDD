db352c5fbae93483b94396b1c5c029480b1a0d24
mchychym <60200816+mchychym@users.noreply.github.com>
2020-03-23 02:47:26 -0800

    final commit

--- string-calculator/calculator.hpp
+++ string-calculator/calculator.hpp
@@ -1,1 +1,8 @@
+#ifndef FILE_CALCULATOR_H_INCLUDED
+#define FILE_CALCULATOR_H_INCLUDED
+#include <string>
 
+int calculate(std::string);
+int calculate(int);
+
+#endif //#ifndef FILE_CALCULATOR_H_INCLUDED
