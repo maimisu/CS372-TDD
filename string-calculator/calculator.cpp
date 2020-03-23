f76251ddd870eff9ffabf70f8e639dee75a673e6
mchychym <60200816+mchychym@users.noreply.github.com>
2020-03-23 02:52:56 -0800

    Update calculator.cpp

--- string-calculator/calculator.cpp
+++ string-calculator/calculator.cpp
@@ -1,1 +1,15 @@
+#include <string>
+#include <sstream>
 
+int calculate(std::string str) {
+	if (str == "") {
+		return 0;
+	}
+	int a;
+	int sum = 0;
+	std::istringstream ssr(str);
+	while (!ssr.eof()) {
+		if (ssr >> a) {
+			sum += a;
+		}
+	}
