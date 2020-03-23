21d7925dd8a0025b23b354b9b1c370139935233c
mchychym <60200816+mchychym@users.noreply.github.com>
2020-03-23 03:23:40 -0800

    Update calculator.hpp

--- string-calculator/calculator.hpp
+++ string-calculator/calculator.hpp
@@ -1,3 +1,8 @@
+// calculator.hpp
+// Malvika Shriwas
+// CS372 string calc TDD
+// Time allotted: 45 minutes
+
 #ifndef FILE_CALCULATOR_H_INCLUDED
 #define FILE_CALCULATOR_H_INCLUDED
 #include <string>
