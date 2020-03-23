1d727c231b58ad445b63a20da3738e56efd249c8
mchychym <60200816+mchychym@users.noreply.github.com>
2020-03-23 03:08:29 -0800

    #1 done!

--- string-calculator/test.cpp
+++ string-calculator/test.cpp
@@ -1,1 +1,12 @@
+#define CATCH_CONFIG_FAST_COMPILE
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+#include "calculator.hpp"
+
+TEST_CASE("empty string returns 0")
+{
+	INFO( "Empty string did not return zero");
+	REQUIRE(0 == calculate(""));
+}
 
